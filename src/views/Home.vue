49d16af793a4e9a3925a75e0e788168f830a189e
amimaro <amir.zahlan@gmail.com>
2019-04-22 21:11:35 -0300

    Display cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="grid-container">
+      <div class="grid-item" v-for="(card, index) in cards" :key="index">
+        <img class="card" :src="card.images.png" :alt="`card-${card.value}`">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState } from 'vuex'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState(['cards'])
   }
 }
 </script>
+
+<style>
+.home {
+  display: table;
+  margin: 0 auto;
+  padding: 2% 0% 0% 2%;
+}
+.card {
+  width: 99%;
+}
+.grid-item {
+  text-align: center;
+}
+.grid-container {
+  display: grid;
+  width: 70vw;
+  height: 100vh;
+  row-gap: 2%;
+  grid-auto-flow: row;
+  grid-template-columns: repeat(7, 10vw);
+  grid-template-rows: repeat(3, 30vh);
+  grid-template-areas: repeat(3, ". . . . . . .");
+}
+@media all and (max-width: 480px) and (min-height: 480px) {
+  .grid-container {
+    width: 100vw;
+    column-gap: 2%;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(3, 30%);
+    grid-template-rows: repeat(7, 10%);
+    grid-template-areas: repeat(7, ". . .");
+  }
+}
+</style>
